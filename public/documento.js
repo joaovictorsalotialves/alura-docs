3f621158a56d8dabaa6ba8713ef5ff7d1d866176
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-24 14:50:30 -0300

    Emitindo eventos

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,7 @@
 const socket = io()
+
+const textArea = document.getElementById('editor-texto')
+
+textArea.addEventListener('keyup', () => {
+  socket.emit('text_change', textArea.value)
+})
--- src/socket-back.js
+++ src/socket-back.js
@@ -2,4 +2,8 @@ import { io } from './server.js'
 
 io.on('connection', (socket) => {
   console.log('A user connected! ID: ', socket.id)
+
+  socket.on('text_change', (text) => {
+    console.log(text)
+  })
 })
