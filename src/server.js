36c983ca78a08b7031cad4e20d269ae9af949fb9
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-23 23:55:43 -0300

    Criando o servidor

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import url from "url"
+import path from "path"
+
+const app = express()
+const port = process.env.SERVER_PORT || 3000 
+
+const currentPath = url.fileURLToPath(import.meta.url)
+const publicPath = path.join(currentPath, '../../public')
+
+app.use(express.static(publicPath))
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
