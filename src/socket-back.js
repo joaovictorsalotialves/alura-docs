40885362773d2c076082c9cec190e5814e9fd3f0
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-25 16:03:41 -0300

    Guardando os dados localmente

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,9 +1,28 @@
 import { io } from './server.js'
 
+const documents = [
+  {
+    name: 'JavaScript',
+    text: 'Texto do documento JavaScript...'
+  },
+  {
+    name: 'Node',
+    text: 'Texto do documento Node...'
+  },
+  {
+    name: 'Socket.io',
+    text: 'Texto do documento Socket.io...'
+  },
+]
+
 io.on('connection', (socket) => {
   console.log('A user connected! ID: ', socket.id)
 
   socket.on('select_document', (nameDocument) => {
+    const document = findDocument(nameDocument)
+
+    console.log(document)
+
     socket.join(nameDocument)
   })
 
@@ -11,3 +30,9 @@ io.on('connection', (socket) => {
     socket.to(nameDocument).emit('text_change_client', text)
   })
 })
+
+function findDocument(nameDocument) {
+  const document = documents.find(doc => doc.name === nameDocument)
+
+  return document
+}
