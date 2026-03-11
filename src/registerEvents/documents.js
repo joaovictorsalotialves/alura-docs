f967cc3d5b22d3370005703558fb3989ed26080c
João Victor <joaovictorsalotialves12@gmail.com>
2026-03-11 14:33:58 -0300

    Eventos para clientes específicos

--- src/registerEvents/documents.js
+++ src/registerEvents/documents.js
@@ -16,21 +16,25 @@ export default function registerEventsDocuments(socket, io) {
 
       returnText(document.text)
     }
-  })
 
-  socket.on('text_change', async ({ text, nameDocument }) => { 
-    const update = await updateDocument(nameDocument, text)
+    socket.on('text_change', async ({ text, nameDocument }) => { 
+      const update = await updateDocument(nameDocument, text)
 
-    if (update.modifiedCount) {
-      socket.to(nameDocument).emit('text_change_client', text)
-    }
-  })
+      if (update.modifiedCount) {
+        socket.to(nameDocument).emit('text_change_client', text)
+      }
+    })
 
-  socket.on('delete_document', async (nameDocument) => {
-    const result = await deleteDocument(nameDocument)
+    socket.on('delete_document', async (nameDocument) => {
+      const result = await deleteDocument(nameDocument)
 
-    if (result.deletedCount) {
-      io.emit('delete_document_success', nameDocument)
-    }
+      if (result.deletedCount) {
+        io.emit('delete_document_success', nameDocument)
+      }
+    })
+
+    socket.on('disconnect', () => {
+      console.log(`Cliente ${socket.id} foi desconectado`)
+    })
   })
 }
